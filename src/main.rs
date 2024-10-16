d5f336755ba129687acfc02908376981183855bf
Fredrik Simonsson <fredrik@nodle.com>
2024-10-17 01:58:42 +0900

    Add parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use marketdata::MarketData;
 use pcap_parser::traits::PcapReaderIterator;
 use pcap_parser::*;
 use std::fs::File;
@@ -13,14 +14,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         match reader.next() {
             Ok((offset, block)) => {
-                println!("got new block");
                 match block {
                     PcapBlockOwned::LegacyHeader(_hdr) => {
                         // save hdr.network (linktype)
                     }
                     PcapBlockOwned::Legacy(b) => {
                         // use linktype to parse b.data()
-                        println!("Hello {:02x?}", b.data)
+                        if let Ok(md) = MarketData::try_from(b.data) {
+                            println!("Hello {} {}", md.data_type(), md.isin())
+                        }
                     }
                     PcapBlockOwned::NG(_) => unreachable!(),
                 }
--- src/marketdata.rs
+++ src/marketdata.rs
@@ -1,66 +1,92 @@
-use serde::Deserialize;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
-struct MarketData {
-    data_type: [u8; 2],                  // B6
-    information_type: [u8; 2],           // 03
-    market_type: u8,                     // 4
-    issue_code: [u8; 12],                // ISIN code
-    issue_seq_no: [u8; 3],               // Issue seq.-no.
-    market_status_type: [u8; 2],         // Market Status Type
-    total_bid_quote_volume: [u8; 7],     // Total bid quote volume
-    best_bid_price_1st: [u8; 5],         // Best bid price (1st)
-    best_bid_quantity_1st: [u8; 7],      // Best bid quantity (1st)
-    best_bid_price_2nd: [u8; 5],         // Best bid price (2nd)
-    best_bid_quantity_2nd: [u8; 7],      // Best bid quantity (2nd)
-    best_bid_price_3rd: [u8; 5],         // Best bid price (3rd)
-    best_bid_quantity_3rd: [u8; 7],      // Best bid quantity (3rd)
-    best_bid_price_4th: [u8; 5],         // Best bid price (4th)
-    best_bid_quantity_4th: [u8; 7],      // Best bid quantity (4th)
-    best_bid_price_5th: [u8; 5],         // Best bid price (5th)
-    best_bid_quantity_5th: [u8; 7],      // Best bid quantity (5th)
-    total_ask_quote_volume: [u8; 7],     // Total ask quote volume
-    best_ask_price_1st: [u8; 5],         // Best ask price (1st)
-    best_ask_quantity_1st: [u8; 7],      // Best ask quantity (1st)
-    best_ask_price_2nd: [u8; 5],         // Best ask price (2nd)
-    best_ask_quantity_2nd: [u8; 7],      // Best ask quantity (2nd)
-    best_ask_price_3rd: [u8; 5],         // Best ask price (3rd)
-    best_ask_quantity_3rd: [u8; 7],      // Best ask quantity (3rd)
-    best_ask_price_4th: [u8; 5],         // Best ask price (4th)
-    best_ask_quantity_4th: [u8; 7],      // Best ask quantity (4th)
-    best_ask_price_5th: [u8; 5],         // Best ask price (5th)
-    best_ask_quantity_5th: [u8; 7],      // Best ask quantity (5th)
-    no_of_best_bid_valid_quote: [u8; 5], // No. of best bid valid quote (total)
-    no_of_best_bid_quote_1st: [u8; 4],   // No. of best bid quote (1st)
-    no_of_best_bid_quote_2nd: [u8; 4],   // No. of best bid quote (2nd)
-    no_of_best_bid_quote_3rd: [u8; 4],   // No. of best bid quote (3rd)
-    no_of_best_bid_quote_4th: [u8; 4],   // No. of best bid quote (4th)
-    no_of_best_bid_quote_5th: [u8; 4],   // No. of best bid quote (5th)
-    no_of_best_ask_valid_quote: [u8; 5], // No. of best ask valid quote (total)
-    no_of_best_ask_quote_1st: [u8; 4],   // No. of best ask quote (1st)
-    no_of_best_ask_quote_2nd: [u8; 4],   // No. of best ask quote (2nd)
-    no_of_best_ask_quote_3rd: [u8; 4],   // No. of best ask quote (3rd)
-    no_of_best_ask_quote_4th: [u8; 4],   // No. of best ask quote (4th)
-    no_of_best_ask_quote_5th: [u8; 4],   // No. of best ask quote (5th)
-    quote_accept_time: [u8; 8],          // HHMMSSuu
-    end_of_message: u8,                  // 0xff
+pub struct MarketData {
+    pub data_type: [u8; 2],                  // B6
+    pub information_type: [u8; 2],           // 03
+    pub market_type: u8,                     // 4
+    pub issue_code: [u8; 12],                // ISIN code
+    pub issue_seq_no: [u8; 3],               // Issue seq.-no.
+    pub market_status_type: [u8; 2],         // Market Status Type
+    pub total_bid_quote_volume: [u8; 7],     // Total bid quote volume
+    pub best_bid_price_1st: [u8; 5],         // Best bid price (1st)
+    pub best_bid_quantity_1st: [u8; 7],      // Best bid quantity (1st)
+    pub best_bid_price_2nd: [u8; 5],         // Best bid price (2nd)
+    pub best_bid_quantity_2nd: [u8; 7],      // Best bid quantity (2nd)
+    pub best_bid_price_3rd: [u8; 5],         // Best bid price (3rd)
+    pub best_bid_quantity_3rd: [u8; 7],      // Best bid quantity (3rd)
+    pub best_bid_price_4th: [u8; 5],         // Best bid price (4th)
+    pub best_bid_quantity_4th: [u8; 7],      // Best bid quantity (4th)
+    pub best_bid_price_5th: [u8; 5],         // Best bid price (5th)
+    pub best_bid_quantity_5th: [u8; 7],      // Best bid quantity (5th)
+    pub total_ask_quote_volume: [u8; 7],     // Total ask quote volume
+    pub best_ask_price_1st: [u8; 5],         // Best ask price (1st)
+    pub best_ask_quantity_1st: [u8; 7],      // Best ask quantity (1st)
+    pub best_ask_price_2nd: [u8; 5],         // Best ask price (2nd)
+    pub best_ask_quantity_2nd: [u8; 7],      // Best ask quantity (2nd)
+    pub best_ask_price_3rd: [u8; 5],         // Best ask price (3rd)
+    pub best_ask_quantity_3rd: [u8; 7],      // Best ask quantity (3rd)
+    pub best_ask_price_4th: [u8; 5],         // Best ask price (4th)
+    pub best_ask_quantity_4th: [u8; 7],      // Best ask quantity (4th)
+    pub best_ask_price_5th: [u8; 5],         // Best ask price (5th)
+    pub best_ask_quantity_5th: [u8; 7],      // Best ask quantity (5th)
+    pub no_of_best_bid_valid_quote: [u8; 5], // No. of best bid valid quote (total)
+    pub no_of_best_bid_quote_1st: [u8; 4],   // No. of best bid quote (1st)
+    pub no_of_best_bid_quote_2nd: [u8; 4],   // No. of best bid quote (2nd)
+    pub no_of_best_bid_quote_3rd: [u8; 4],   // No. of best bid quote (3rd)
+    pub no_of_best_bid_quote_4th: [u8; 4],   // No. of best bid quote (4th)
+    pub no_of_best_bid_quote_5th: [u8; 4],   // No. of best bid quote (5th)
+    pub no_of_best_ask_valid_quote: [u8; 5], // No. of best ask valid quote (total)
+    pub no_of_best_ask_quote_1st: [u8; 4],   // No. of best ask quote (1st)
+    pub no_of_best_ask_quote_2nd: [u8; 4],   // No. of best ask quote (2nd)
+    pub no_of_best_ask_quote_3rd: [u8; 4],   // No. of best ask quote (3rd)
+    pub no_of_best_ask_quote_4th: [u8; 4],   // No. of best ask quote (4th)
+    pub no_of_best_ask_quote_5th: [u8; 4],   // No. of best ask quote (5th)
+    pub quote_accept_time: [u8; 8],          // HHMMSSuu
+    pub end_of_message: u8,                  // 0xff
 }
 
-// impl TryFrom<&[u8]> for MarketData{
-//     type Error;
+impl MarketData {
+    pub fn isin(&self) -> Cow<'_,str> {
+        String::from_utf8_lossy(&self.issue_code)
+    }
+    pub fn data_type(&self) -> Cow<'_,str>{
+        String::from_utf8_lossy(&self.data_type)
+    }
+}
+#[derive(thiserror::Error, Debug)]
+pub enum MDError {
+    #[error("Data format not supported")]
+    DataTypeUnknown,
+    #[error("Data of size {0} not supported")]
+    DataSizeUnsupported(usize),
+    #[error("bincode::ErrorKind {0}")]
+    BinCode(#[from] Box<bincode::ErrorKind>),
+}
 
-//     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-//         value.len()== 257;
-//         let decoded: MarketData = bincode::deserialize(&value[85..257])?;
-//         assert_eq!(decoded.data_type, [0x42, 0x36]);
-//         assert_eq!(decoded.information_type, [0x30, 0x33]);
-//         assert_eq!(decoded.end_of_message, 0xff);
+impl TryFrom<&[u8]> for MarketData {
+    type Error = MDError;
 
-//     }
-// }
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        if value.len() != 257 {
+            return Err(MDError::DataSizeUnsupported(value.len()));
+        }
+        let decoded: MarketData = bincode::deserialize(&value[42..257])?;
+        if decoded.data_type != [0x42, 0x36] {
+            return Err(MDError::DataTypeUnknown);
+        };
+        if decoded.information_type != [0x30, 0x33] {
+            return Err(MDError::DataTypeUnknown);
+        }
+        if decoded.end_of_message != 0xff {
+            return Err(MDError::DataTypeUnknown);
+        }
+        Ok(decoded)
+    }
+}
 #[test]
-fn parse_market_data() {
+fn parse_market_data_from_clean_bytes() {
     let encoded: [u8; 215] = [
         0x42, 0x36, 0x30, 0x33, 0x34, 0x4b, 0x52, 0x34, 0x33, 0x30, 0x31, 0x46, 0x33, 0x32, 0x35,
         0x37, 0x30, 0x30, 0x30, 0x34, 0x34, 0x30, 0x30, 0x30, 0x31, 0x31, 0x31, 0x32, 0x33, 0x30,
@@ -82,4 +108,25 @@ fn parse_market_data() {
     assert_eq!(decoded.data_type, [0x42, 0x36]);
     assert_eq!(decoded.information_type, [0x30, 0x33]);
     assert_eq!(decoded.end_of_message, 0xff);
+}
+
+#[test]
+fn try_from_for_full_udp() {
+    let fulludp = [
+        1u8, 0, 94, 37, 54, 61, 0, 18, 68, 200, 56, 10, 8, 0, 69, 0, 0, 243, 11, 241, 0, 0, 59, 17,
+        145, 136, 192, 166, 1, 120, 233, 37, 54, 61, 141, 203, 60, 155, 0, 223, 40, 57, 66, 54, 48,
+        51, 52, 75, 82, 52, 50, 48, 49, 70, 51, 50, 55, 50, 49, 48, 48, 50, 52, 48, 48, 48, 50, 48,
+        55, 48, 54, 48, 48, 49, 52, 50, 48, 48, 48, 48, 48, 48, 53, 48, 48, 49, 52, 49, 48, 48, 48,
+        48, 48, 55, 53, 48, 48, 49, 52, 48, 48, 48, 48, 48, 54, 54, 56, 48, 48, 49, 51, 57, 48, 48,
+        48, 48, 50, 52, 54, 48, 48, 49, 51, 56, 48, 48, 48, 48, 53, 49, 57, 48, 48, 48, 52, 53, 49,
+        53, 48, 48, 49, 52, 51, 48, 48, 48, 48, 48, 51, 57, 48, 48, 49, 52, 52, 48, 48, 48, 48, 48,
+        54, 50, 48, 48, 49, 52, 53, 48, 48, 48, 48, 48, 55, 56, 48, 48, 49, 52, 54, 48, 48, 48, 48,
+        48, 55, 52, 48, 48, 49, 52, 55, 48, 48, 48, 48, 48, 55, 53, 48, 48, 55, 55, 56, 48, 48, 48,
+        52, 48, 48, 48, 56, 48, 48, 50, 49, 48, 48, 49, 52, 48, 48, 53, 48, 48, 48, 51, 48, 55, 48,
+        48, 48, 52, 48, 48, 48, 52, 48, 48, 49, 48, 48, 48, 48, 51, 48, 48, 48, 51, 48, 57, 48, 48,
+        50, 57, 57, 55, 255,
+    ];
+
+    let md: Result<MarketData, _> = fulludp.try_into();
+    assert!(md.is_ok())
 }
